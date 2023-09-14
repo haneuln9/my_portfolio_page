19f7ecf0664c448514ecfb21b3eaaf5f065075ab
haneul <sky9196@naver.com>
2023-09-14 17:38:31 +0900

    end

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// const abt1 = document.querySelector ('#tab_sn1')
+// const abt2 = document.querySelector ('#tab_sn2')
+// const abt3 = document.querySelector ('#tab_sn3')
+// const sn1 = document.querySelector ('#section_num1')
+// const sn2 = document.querySelector ('#section_num2')
+// const sn3 = document.querySelector ('#section_num3')
+// console.log(abt1,abt2,abt3,sn1,sn2,sn3)
+// // abt1.forEach(function(t,i){
+// //     t.addEventListener('click',function(e){
+// //         e.preventDefault()
+// //         window.scrollTo(0,sn1.offsetTop)
+// //     })
+// // })
+// abt1.addEventListener
+// re_img.addEventListener('mouseover',function(e){
+//     re_img_txt.style.display = 'block'
+// })
+// re_img.addEventListener('mouseout',function(e){
+//     re_img_txt.style.display = 'none'
+// })
